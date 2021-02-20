7d607087d03b55de6530e185c6cedae00d3a7e4d
akgunduz5434 <akgunduz5434@gmail.com>
2021-02-20 09:41:31 +0300

    class yapısı

---
+++ OOP/1_class_yapisi.dart
@@ -0,0 +1,10 @@
+// Class 'lar bizim oluşturduğumuz veri tipleridir. Yani int, string gibi...
+// Class isimlerinin ilk harfi büyük harfle başlamalıdır.
+class Ogrenci {
+
+  // Önce sınıfın  özelliklerini tanımlayalım.
+  // Bunlara özellik - instance veya field variable denir.
+  int ogrNo;
+  String ogrAdiSoyadi;
+  bool bursAliyormu = true;
+}
